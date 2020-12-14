d5bd9191731320393b87c39c2253bba52311de66
camwalk <55843608+camwalk@users.noreply.github.com>
2020-12-14 23:14:44 +0000

    reformatted books display

--- client-app/src/app/models/book.ts
+++ client-app/src/app/models/book.ts
@@ -2,7 +2,7 @@
     export interface Book
     {
         id: number;
-        author: string;
+        authors: string;
         title: string;
         description: string;
         publisher: string;
